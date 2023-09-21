138fa6e4eaa7e3ef014eff89a0596236a281f2ba
kukoo <96556724+Kukoomomo@users.noreply.github.com>
2023-09-21 17:29:57 +0800

    Add check (#1)
    * add check
    
    * add go mod
    
    * add go mod
    
    * add go mod
    
    * add go check
    
    * err timestamp check
    
    * fix
    
    * update

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/xeipuuv/gojsonschema"
+)
+
+const (
+	SchemaFilePath       = "./src/token.schema.json"
+	DevNetTokenListPath  = "./src/devnet/tokenList.json"
+	TestNetTokenListPath = "./src/testnet/tokenList.json"
+)
+
+var CheckPath = [...]string{
+	DevNetTokenListPath,
+	TestNetTokenListPath,
+}
+
+func main() {
+	// get time now
+	currentTime := time.Now().UTC().Format(time.RFC3339)
+	_ = currentTime
+	schemaLoader := gojsonschema.NewReferenceLoader("file://" + SchemaFilePath)
+	for i := 0; i < len(CheckPath); i++ {
+		tokenListPath := CheckPath[i]
+		documentLoader := gojsonschema.NewReferenceLoader("file://" + tokenListPath)
+
+		result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		if result.Valid() {
+			fmt.Printf("The document is valid\n")
+		} else {
+			fmt.Printf("The document is not valid. see errors :\n")
+			for _, desc := range result.Errors() {
+				fmt.Printf("- %s\n", desc)
+			}
+			panic("check ERR")
+		}
+	}
+
+}
