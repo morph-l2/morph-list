8deca0f7bb88f56f08034a0ad4b1d9a595733e66
kukoomomo <kukoomomo@gmail.com>
2023-09-21 16:54:59 +0800

    add go check

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
 
+	"github.com/xeipuuv/gojsonschema"
+)
+
+func main() {
+	schemaLoader := gojsonschema.NewReferenceLoader("file://./src/schema.json")
+	documentLoader := gojsonschema.NewReferenceLoader("file://./src/list.json")
+
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// get time now
+	currentTime := time.Now().UTC().Format(time.RFC3339)
+	_ = currentTime
+
+	if result.Valid() {
+		fmt.Printf("The document is valid\n")
+	} else {
+		fmt.Printf("The document is not valid. see errors :\n")
+		for _, desc := range result.Errors() {
+			fmt.Printf("- %s\n", desc)
+		}
+		panic("check ERR")
+	}
 }
